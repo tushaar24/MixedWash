80173013c64fbb390bb82d3a4f2f114b2a1d628c
Stillswarm <abhi555sri@gmail.com>
2025-03-25 00:19:33 +0530

    feat(address_list_screen): refactored to show a "no saved addresses" messages if the address list is empty

--- composeApp/src/commonMain/kotlin/com/mixedwash/features/address/presentation/components/AddressList.kt
+++ composeApp/src/commonMain/kotlin/com/mixedwash/features/address/presentation/components/AddressList.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,17 +23,19 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mixedwash.features.address.domain.model.Address
 import com.mixedwash.features.address.presentation.AddressSearchEvent
 import com.mixedwash.features.address.presentation.AddressSearchState
-import com.mixedwash.features.address.domain.model.Address
 import com.mixedwash.ui.theme.MixedWashTheme
 import com.mixedwash.ui.theme.cardSpacing
 import com.mixedwash.ui.theme.components.IconButton
@@ -45,7 +49,7 @@ fun AddressList(
     selectedAddressId: String? = null,
     onAddressClicked: ((String) -> Unit)? = null,
     onAddressEdit: ((Address) -> Unit)? = null,
-    addressSearchState: AddressSearchState?=null
+    addressSearchState: AddressSearchState? = null
 ) {
     LazyColumn(
         modifier = modifier.animateContentSize(),
@@ -59,7 +63,7 @@ fun AddressList(
                     placeHolder = state.placeHolder,
                     enabled = state.enabled,
                     places = state.autocompleteResult,
-                    onValueChange = {state.onEvent(AddressSearchEvent.OnValueChange(it))},
+                    onValueChange = { state.onEvent(AddressSearchEvent.OnValueChange(it)) },
                     onLocationClick = { state.onEvent(AddressSearchEvent.OnLocationClick) },
                     onPlaceSelected = { state.onEvent(AddressSearchEvent.OnPlaceSelected(it)) },
                     onClearRequest = { state.onEvent(AddressSearchEvent.OnClear) },
@@ -69,69 +73,86 @@ fun AddressList(
             }
         }
 
-        items(count = addresses.size, key = { index -> addresses[index].uid }) { index ->
-
-            val address = addresses[index]
-            val isSelected = selectedAddressId == address.uid
-
-            val borderColor = if (isSelected) Color.Transparent
-            else Color.Transparent
-
-            val containerColor = if (isSelected) BrandTheme.colors.gray.c300
-            else Color.Transparent
-
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .border(
-                    1.dp, color = borderColor, shape = BrandTheme.shapes.card
-                )
-                .clip(BrandTheme.shapes.card)
-                .background(containerColor)
-                .clickable(enabled = onAddressClicked != null && !isSelected) {
-                    onAddressClicked!!(
-                        address.uid
+        if (addresses.isEmpty()) {
+            item {
+                Box(
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.TopCenter
+                ) {
+                    Text(
+                        text = "No Saved Addresses!",
+                        fontSize = 12.sp,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
                     )
                 }
-                .padding(horizontal = 16.dp, vertical = 16.dp)) {
-                Icon(
-                    imageVector = Icons.Rounded.LocationOn,
-                    contentDescription = "Location Icon",
-                    tint = BrandTheme.colors.gray.dark,
-                    modifier = Modifier
-                        .padding(top = 4.dp, end = 18.dp)
-                        .size(14.dp)
-                )
-                Column(modifier = Modifier.weight(1f)) {
-                    Text (
-                        text = address.title,
-                        style = BrandTheme.typography.subtitle2,
-                        color = BrandTheme.colors.gray.dark
-                    )
+            }
+        } else {
+            items(count = addresses.size, key = { index -> addresses[index].uid }) { index ->
 
-                    Spacer(Modifier.height(8.dp))
-                    Text(
-                        text = address.run {
-                            """
+                val address = addresses[index]
+                val isSelected = selectedAddressId == address.uid
+
+                val borderColor = if (isSelected) Color.Transparent
+                else Color.Transparent
+
+                val containerColor = if (isSelected) BrandTheme.colors.gray.c300
+                else Color.Transparent
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .border(
+                            1.dp, color = borderColor, shape = BrandTheme.shapes.card
+                        )
+                        .clip(BrandTheme.shapes.card)
+                        .background(containerColor)
+                        .clickable(enabled = onAddressClicked != null && !isSelected) {
+                            onAddressClicked!!(
+                                address.uid
+                            )
+                        }
+                        .padding(horizontal = 16.dp, vertical = 16.dp)) {
+                    Icon(
+                        imageVector = Icons.Rounded.LocationOn,
+                        contentDescription = "Location Icon",
+                        tint = BrandTheme.colors.gray.dark,
+                        modifier = Modifier
+                            .padding(top = 4.dp, end = 18.dp)
+                            .size(14.dp)
+                    )
+                    Column(modifier = Modifier.weight(1f)) {
+                        Text(
+                            text = address.title,
+                            style = BrandTheme.typography.subtitle2,
+                            color = BrandTheme.colors.gray.dark
+                        )
+
+                        Spacer(Modifier.height(8.dp))
+                        Text(
+                            text = address.run {
+                                """
                                     $addressLine1, $addressLine2 $addressLine3 
                                     $pinCode
                                 """.trimIndent()
-                        },
-                        style = BrandTheme.typography.body2,
-                        color = BrandTheme.colors.gray.darker
-                    )
+                            },
+                            style = BrandTheme.typography.body2,
+                            color = BrandTheme.colors.gray.darker
+                        )
 
-                }
-                Spacer(modifier = Modifier.padding(16.dp))
+                    }
+                    Spacer(modifier = Modifier.padding(16.dp))
 
-                onAddressEdit?.let {
-                    IconButton(
-                        iconSize = 16.dp,
-                        imageVector = Icons.Rounded.MoreVert,
-                        onClick = { it(address) },
-                        enabled = true,
-                        buttonColors = BrandTheme.colors.iconButtonColors()
-                            .copy(containerColor = Color.Transparent)
-                    )
+                    onAddressEdit?.let {
+                        IconButton(
+                            iconSize = 16.dp,
+                            imageVector = Icons.Rounded.MoreVert,
+                            onClick = { it(address) },
+                            enabled = true,
+                            buttonColors = BrandTheme.colors.iconButtonColors()
+                                .copy(containerColor = Color.Transparent)
+                        )
+                    }
                 }
             }
         }
@@ -167,7 +188,7 @@ private fun PreviewAddressList() {
         )
     )
 
-    var selectedId by remember {mutableStateOf("")}
+    var selectedId by remember { mutableStateOf("") }
     MixedWashTheme {
         AddressList(
             modifier = Modifier
