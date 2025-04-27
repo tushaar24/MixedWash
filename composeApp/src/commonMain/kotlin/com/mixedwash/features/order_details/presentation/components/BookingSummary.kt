b4a9adacc68f768219c7f61d0949e3c0052812b9
xidl <26184016+xidsyed@users.noreply.github.com>
2025-04-28 00:23:01 +0530

    feat: update booking model to use payment id instead of paid status and add user type to user metadata

--- composeApp/src/commonMain/kotlin/com/mixedwash/features/order_details/presentation/components/BookingSummary.kt
+++ composeApp/src/commonMain/kotlin/com/mixedwash/features/order_details/presentation/components/BookingSummary.kt
@@ -192,7 +192,7 @@ fun BookingSummary(
                         )
                     }
 
-                    if (!booking.isPaid) {
+                    if (booking.paymentId == null) {
                         Box(
                             modifier = Modifier.clip(RoundedCornerShape(6.dp))
                                 .background(BrandTheme.colors.gray.darker)
