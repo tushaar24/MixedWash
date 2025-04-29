0a2e50faa4f802bf954397ca6180da044c46617c
xidl <26184016+xidsyed@users.noreply.github.com>
2025-04-29 23:34:26 +0530

    refactor: add delivery address to booking data model and service

--- composeApp/src/commonMain/kotlin/com/mixedwash/core/feature/orders/data/model/Mapper.kt
+++ composeApp/src/commonMain/kotlin/com/mixedwash/core/feature/orders/data/model/Mapper.kt
@@ -2,9 +2,10 @@ package com.mixedwash.core.feature.orders.data.model
 
 import com.mixedwash.core.feature.orders.domain.model.Booking
 import com.mixedwash.core.feature.orders.domain.model.Order
+import com.mixedwash.features.address.domain.model.Address
 
 
-fun com.mixedwash.core.feature.orders.data.model.OrderDto.toOrder(bookings: List<Booking>) = Order(
+fun OrderDto.toOrder(bookings: List<Booking>) = Order(
     id = id,
     bookings = bookings,
     createdAtSeconds = createdAtSeconds,
@@ -15,7 +16,7 @@ fun com.mixedwash.core.feature.orders.data.model.OrderDto.toOrder(bookings: List
     address = address
 )
 
-fun Order.toOrderDto(userId: String) = com.mixedwash.core.feature.orders.data.model.OrderDto(
+fun Order.toOrderDto(userId: String) = OrderDto(
     id = id,
     userId = userId,
     createdAtSeconds = createdAtSeconds,
@@ -26,10 +27,17 @@ fun Order.toOrderDto(userId: String) = com.mixedwash.core.feature.orders.data.mo
     address = address
 )
 
-fun Booking.toBookingDto(orderId: String, userId: String, createdAtSeconds: Long) = BookingDto(
+fun Booking.toBookingDto(
+    orderId: String,
+    userId: String,
+    createdAtSeconds: Long,
+    deliveryAddress: Address
+) = BookingDto(
     id = id,
     userId = userId,
     orderId = orderId,
+    createdAtSeconds = createdAtSeconds,
+    deliveryAddress = deliveryAddress,
     pickupSlotSelected = pickupSlotSelected,
     dropSlotSelected = dropSlotSelected,
     bookingItems = bookingItems,
@@ -37,9 +45,8 @@ fun Booking.toBookingDto(orderId: String, userId: String, createdAtSeconds: Long
     deliveredSeconds = deliveredSeconds,
     paymentId = paymentId,
     isCancelled = isCancelled,
-    cancellationReason = cancellationReason,
-    createdAtSeconds = createdAtSeconds,
-    activeCancellationRequestPlacedSeconds = activeCancellationRequestPlacedSeconds
+    activeCancellationRequestPlacedSeconds = activeCancellationRequestPlacedSeconds,
+    cancellationReason = cancellationReason
 )
 
 fun BookingDto.toBooking() = Booking(
